5d0ccec8c1fa8a1a67cc2fbb24364254d42669a6
yadavritika <158076203+yadavritika@users.noreply.github.com>
2024-01-31 09:25:16 +0530

    Delete minor project directory

--- minor project/js/resp.js
+++
@@ -1,9 +0,0 @@
-burger=document.querySelector('.burger')
-navbar=document.querySelector('.navbar')
-navList=document.querySelector('.nav-list')
-rightNav=document.querySelector('.rightNav')
-burger.addEventListener('click',()=>{
-    rightNav.classList.toggle('v-class')
-    navList.classList.toggle('v-class')
-    navbar.classList.toggle('h-nav')
-})
